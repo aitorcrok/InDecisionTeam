70a8f45047d97c4b77853c5e797fc832b8373162
aitorcrok <aitgar03@ucm.es>
2019-10-18 18:45:54 +0200

    jemplei

--- game.js
+++ game.js
@@ -8,13 +8,19 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
-    //d = this.input.keyboard.addKey('D');
-    //this.add.image(100, 100, 'testo');
-    //pureeya = this.add.sprite(100, 400, 'testo');
     var senor = new Player(this);
+    this.d = this.input.keyboard.addKey('D');
+    this.a = this.input.keyboard.addKey('A');
+    this.d.on('down', event => {senor.speedX = 5});
+    this.a.on('down', event => {senor.speedX = -5});
   }
 
   update(time, delta) {
-    //if(d.isDown){senor.y =+ 10};
-  }
+  //   if(!this.a.isDown && !this.d.isDown){
+  //     if(Phaser.Input.Keyboard.JustUp(this.a) || Phaser.Input.Keyboard.JustUp(this.a)){
+  //       senor.speedX = 0;
+  //     }
+  //    }
+
+  // }
 }
