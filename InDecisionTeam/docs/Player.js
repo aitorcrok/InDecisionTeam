7ce015a4d9b33c25a26b9e8d92a12d50f080665c
aitorcrok <aitgar03@ucm.es>
2019-10-25 17:30:33 +0200

    1001 dálmatas

---
+++ InDecisionTeam/docs/Player.js
@@ -0,0 +1,18 @@
+import Ship from '/InDecisionTeam/ship.js';
+export default class Player extends Ship{
+    constructor(scene){
+        super(scene, 400, 400, 300, 'testo')
+        this.d = this.scene.input.keyboard.addKey('D');
+        this.a = this.scene.input.keyboard.addKey('A');
+        this.s = this.scene.input.keyboard.addKey('S');
+        this.w = this.scene.input.keyboard.addKey('W');
+        this.d.on('down', event => {this.setVelocityX(1)});
+        this.a.on('down', event => {this.setVelocityX(-1)});
+        this.s.on('down', event => {this.setVelocityY(1)});
+        this.w.on('down', event => {this.setVelocityY(-1)});
+        this.d.on('up', event => {if(this.body.velocity.x > 0)this.setVelocityX(0)});
+        this.a.on('up', event => {if(this.body.velocity.x < 0)this.setVelocityX(0)});
+        this.s.on('up', event => {if(this.body.velocity.y > 0)this.setVelocityY(0)});
+        this.w.on('up', event => {if(this.body.velocity.y < 0)this.setVelocityY(0)});
+    }//2
+}
---
+++ InDecisionTeam/docs/ship.js
@@ -0,0 +1,20 @@
+export default class Ship extends Phaser.GameObjects.Sprite{
+    constructor(scene, newX, newY, newSpeed, sprite){
+        let x = newX;
+        let y = newY;
+        super(scene, x, y, sprite);
+        this.scene.add.existing(this);
+        this.scene.physics.add.existing(this);
+        this.speed = newSpeed;
+    }
+    preUpdate(){
+        // if(this.y > 800){this.y = 0; this.x = Math.random() * 1400;this.speed = Math.random()*10;} 
+        // if(this.x > 1400)this.x = 0;
+    }
+    setVelocityX(x){
+        this.body.setVelocity(this.speed * x, this.body.velocity.y);
+    }
+    setVelocityY(y){
+        this.body.setVelocity(this.body.velocity.x, this.speed * y);
+    }
+}
