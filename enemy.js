22bb1c7ecf03a99525032310dd93a39212f431a0
AitorGarcia <aitorcrok>
2019-11-07 00:28:25 +0100

    timer

--- enemy.js
+++ enemy.js
@@ -5,13 +5,22 @@ export default class Enemy extends Ship{
         super(scene, 500, 500, 0, 'enemy', 3);
         this.setScale(0.1);
         this.time = 0;
+        this.startShoot();
+        }
+    startShoot(){
+        console.log("start shooting");
+        this.timer = this.scene.time.addEvent({
+            delay: 1000,
+            callback: this.shoot,
+            callbackScope: this,
+            loop: true
+        });
     }
-    preUpdate(time, delta){
-        if(this.time == 0){
-            this.time = time;
-        }
-        if(time > this.time + 100){
-            var bu = new Bullet(this.scene, this.x, this.y, 'bullet')
-        }
+    shoot(){
+        console.log("shoooot");
+        var bu = new Bullet(this.scene, this.x, this.y, 'bullet')
+    }
+    stopShoot(){
+        this.timer.remove();
     }
 }
